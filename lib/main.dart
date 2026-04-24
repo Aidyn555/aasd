eb32d42028b8b76d1e53c0db86669f34831b2beb
Yoe <alisherza5555@gmail.com>
2026-04-24 18:27:34 +0500

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Title("A", HitType.hit),
+        body: Center(child: Title("6 + 7 ", HitType.hit)),
+        appBar: AppBar(
+          title: const Align(
+            alignment: Alignment.centerLeft,
+            child: Text("THE BOYS"),
+          ),
         ),
       ),
     );
@@ -29,19 +33,18 @@ class Title extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 60,
-      height: 60,
+      width: 100,
+      height: 100,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey.shade300),
         color: switch (hitType) {
-          HitType.hit => Colors.green,
-          HitType.miss => Colors.grey, 
-          HitType.partial => Colors.yellow, 
-          _ => Colors.white}),
-   child: Center(child:Text(letter)
-    
-    ),
-    );  
+          HitType.hit => const Color.fromARGB(255, 52, 196, 60),
+          HitType.miss => Colors.grey,
+          HitType.partial => Colors.yellow,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(child: Text(letter)),
+    );
   }
 }
- 
